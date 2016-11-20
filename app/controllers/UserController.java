5255f66bdac31ffc25a3bf7af923f2efd578cf35
rajgopal <raj.gopal980@gmail.com>
2016-11-20 11:59:24 +0530

    api creation for saving user data in database

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import com.fasterxml.jackson.databind.JsonNode;
+import model.User;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 import views.html.search;
 import views.html.user;
@@ -11,6 +15,26 @@ import views.html.user;
 public class UserController extends Controller {
 
     public Result userUI() { return ok(user.render()); }
+
+    public Result addUser() {
+
+        //POST Body
+        Http.RequestBody body = request().body();
+        JsonNode json = body.asJson();
+        String firstName = json.findPath("firstName").asText();
+        String lastName = json.findPath("lastName").asText();
+        int age = json.findPath("age").asInt();
+        String dob = json.findPath("firstName").asText();
+        String gender = json.findPath("firstName").asText();
+        int contact = json.findPath("firstName").asInt();
+        String info = json.findPath("firstName").asText();
+
+        User user = new User(firstName, lastName, age, dob, gender, contact, info);
+        Ebean.save(user);
+        return ok("okk");
+    }
+
+    //searchUser
 
     public Result searchUI() { return ok(search.render()); }
 }
---
+++ app/model/User.java
@@ -0,0 +1,70 @@
+package model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by rajgopal on 20/11/16.
+ */
+
+@Entity
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String dob;
+    private String gender;
+    private int contact;
+    private String info;
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public int getAge() { return age; }
+
+    public void setAge(int age) { this.age = age; }
+
+    public String getDob() { return dob; }
+
+    public void setDob(String dob) { this.dob = dob; }
+
+    public String getGender() { return gender; }
+
+    public void setGender(String gender) { this.gender = gender; }
+
+    public int getContact() { return contact; }
+
+    public void setContact(int contact) { this.contact = contact; }
+
+    public String getInfo() { return info; }
+
+    public void setInfo(String info) { this.info = info; }
+
+    public User(String firstName, String lastName, int age, String dob, String gender, int contact, String info) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.dob = dob;
+        this.gender = gender;
+        this.contact = contact;
+        this.info = info;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", dob='" + dob + '\'' +
+                ", gender='" + gender + '\'' +
+                ", contact=" + contact +
+                ", info='" + info + '\'' +
+                '}';
+    }
+}
